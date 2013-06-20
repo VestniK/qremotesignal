d16a65ccd5dd37145a88ce9ffc492b826355c719
Sergey Vidyuk <sir.vestnik@gmail.com>
2013-06-21 01:07:39 +0700

    Coding stile changes in the qrsc.cpp
     * Consistent indentation style in whole file
     * Remove additional brackets
     * Remove additional spaces

--- qrsc/qrsc.cpp
+++ qrsc/qrsc.cpp
@@ -26,130 +26,137 @@ const QString SOURCE_OPTION = "source";
 const QString UPDATE_OPTION = "update";
 
 ArgvConf qrscArgConf = {
-   QT_TRANSLATE_NOOP("ArgvParser", "QRemoteSignal interface compiler"),
-   VERSION,
-   QT_TRANSLATE_NOOP(
-      "ArgvParser",
-      "QRemoteSignal interface compiler. Creates client or service source "
-      "files from interface XML description. Only one of the --service or "
-      "--client flags should be specified but not both."
+    QT_TRANSLATE_NOOP("ArgvParser", "QRemoteSignal interface compiler"),
+    VERSION,
+    QT_TRANSLATE_NOOP(
+        "ArgvParser",
+        "QRemoteSignal interface compiler. Creates client or service source "
+        "files from interface XML description. Only one of the --service or "
+        "--client flags should be specified but not both."
     )
 };
 
-int main(int argc, char *argv[]) {
-   // Application Info
-   QCoreApplication app(argc,argv);
+int main(int argc, char *argv[])
+{
+    // Application Info
+    QCoreApplication app(argc,argv);
 
-   QString locale = QLocale::system().name();
-   QTranslator translator;
-   QString i18nFile = QString("qrsc-%1").arg(locale);
-   if (!translator.load(i18nFile,TRANSLATIONS_DIR)) {
-       translator.load(i18nFile,app.applicationDirPath()+"/../share/i18n");
-   }
-   app.installTranslator(&translator);
+    QString locale = QLocale::system().name();
+    QTranslator translator;
+    QString i18nFile = QString("qrsc-%1").arg(locale);
+    if (!translator.load(i18nFile,TRANSLATIONS_DIR))
+        translator.load(i18nFile,app.applicationDirPath()+"/../share/i18n");
+    app.installTranslator(&translator);
 
-   QTextStream out(stdout,QIODevice::WriteOnly);
-   QTextStream err(stderr,QIODevice::WriteOnly);
-   ArgvParser conf(qrscArgConf, &out, &err);
-   // Command line options configuration
-   conf.addUsageDescription(ArgvParser::tr("--%1|--%2 [OPTIONS] INTERFACE")
-                                .arg(SERVICE_FLAG)
-                                .arg(CLIENT_FLAG));
-   conf.addUsageDescription(ArgvParser::tr("--%1 SOURCE DEST")
-                                .arg(UPDATE_OPTION));
+    QTextStream out(stdout,QIODevice::WriteOnly);
+    QTextStream err(stderr,QIODevice::WriteOnly);
+    ArgvParser conf(qrscArgConf, &out, &err);
+    // Command line options configuration
+    conf.addUsageDescription(ArgvParser::tr("--%1|--%2 [OPTIONS] INTERFACE")
+        .arg(SERVICE_FLAG)
+        .arg(CLIENT_FLAG)
+    );
+    conf.addUsageDescription(ArgvParser::tr("--%1 SOURCE DEST")
+        .arg(UPDATE_OPTION)
+    );
 
-   conf.addFlag(SERVICE_FLAG,ArgvParser::tr("Create service class."),'s');
-   conf.addFlag(CLIENT_FLAG,ArgvParser::tr("Create client class."),'c');
-   conf.addOption(UPDATE_OPTION,
-                  ArgvParser::tr("SOURCE DEST"),
-                  ArgvParser::tr("Update interface file of the format used by the "
-                          "library version 0.6.0 and earlier to the current "
-                          "version of the interface description file format."),
-                  'u');
-   conf.addOption(HEADER_OPTION,
-                  ArgvParser::tr("DEST_HEADER"),
-                  ArgvParser::tr("Specify output header file."));
-   conf.addOption(SOURCE_OPTION,
-                  ArgvParser::tr("DEST_CPP"),
-                  ArgvParser::tr("Specify output C++ source file."));
+    conf.addFlag(SERVICE_FLAG,ArgvParser::tr("Create service class."),'s');
+    conf.addFlag(CLIENT_FLAG,ArgvParser::tr("Create client class."),'c');
+    conf.addOption(
+        UPDATE_OPTION,
+        ArgvParser::tr("SOURCE DEST"),
+        ArgvParser::tr(
+            "Update interface file of the format used by the "
+            "library version 0.6.0 and earlier to the current "
+            "version of the interface description file format."
+        ),
+        'u'
+    );
+    conf.addOption(
+        HEADER_OPTION,
+        ArgvParser::tr("DEST_HEADER"),
+        ArgvParser::tr("Specify output header file.")
+    );
+    conf.addOption(
+        SOURCE_OPTION,
+        ArgvParser::tr("DEST_CPP"),
+        ArgvParser::tr("Specify output C++ source file.")
+    );
     if (!conf.parse())
         return 1;
     // Print info if requested
     if (conf.handleHelp())
         return 0;
 
-    if ( conf.arguments().isEmpty() ) {
-        if ( conf.options()[UPDATE_OPTION].isEmpty() ) {
+    if (conf.arguments().isEmpty()) {
+        if ( conf.options()[UPDATE_OPTION].isEmpty() )
             err << ArgvParser::tr("Error: Input file not specified!") << endl;
-        } else {
+        else
             err << ArgvParser::tr("Error: Destination file not specified!") << endl;
-        }
         out << conf.helpStr();
         return 1;
     }
 
-   // Update document
-   if ( ! conf.options()[UPDATE_OPTION].isEmpty() ) {
-      InterfaceDocument inputDoc( conf.options()[UPDATE_OPTION] );
-      QFile out(conf.arguments().first());
-      out.open(QIODevice::WriteOnly | QIODevice::Text);
-      inputDoc.getIODevice()->seek(0);
-      out.write( inputDoc.getIODevice()->readAll() );
-      return 0;
-   }
+    // Update document
+    if (!conf.options()[UPDATE_OPTION].isEmpty()) {
+        InterfaceDocument inputDoc(conf.options()[UPDATE_OPTION]);
+        QFile out(conf.arguments().first());
+        out.open(QIODevice::WriteOnly | QIODevice::Text);
+        inputDoc.getIODevice()->seek(0);
+        out.write( inputDoc.getIODevice()->readAll() );
+        return 0;
+    }
 
-   InterfaceDocument inputDoc( conf.arguments().first() );
-   if ( ! inputDoc.isValid() ) {
-      err << inputDoc.error() << endl;
-      return 1;
-   }
-   // Compile document
-   InterfaceCompiler compiler(&inputDoc);
+    InterfaceDocument inputDoc(conf.arguments().first());
+    if (!inputDoc.isValid()) {
+        err << inputDoc.error() << endl;
+        return 1;
+    }
+    // Compile document
+    InterfaceCompiler compiler(&inputDoc);
 
-   /// @todo move output files to the compiler class.
-   QString header = conf.options()[HEADER_OPTION];
-   QString source = conf.options()[SOURCE_OPTION];
-   if ( conf.flags()[SERVICE_FLAG] && conf.flags()[CLIENT_FLAG] ) {
-      err << ArgvParser::tr("You should specify --%1 or --%2 flag but not both!")
-                 .arg(SERVICE_FLAG).arg(CLIENT_FLAG) << endl;
-      return 1;
-   }
-   if ( conf.flags()[SERVICE_FLAG] ) {
-      if ( ! header.isEmpty() ) {
-         inputDoc.setServiceHeader(header);
-      }
-      if ( ! source.isEmpty() ) {
-         inputDoc.setServiceSource(source);
-      }
-      if ( !compiler.compileServiceHeader() ) {
-         err << QCoreApplication::tr("Failed to compile the interface!") << endl;
-         return 1;
-      }
-      if ( !compiler.compileServiceSource() ) {
-         err << QCoreApplication::tr("Failed to compile the interface!") << endl;
-         return 1;
-      }
-   } else if ( conf.flags()[CLIENT_FLAG] ) {
-      if ( ! header.isEmpty() ) {
-         inputDoc.setClientHeader(header);
-      }
-      if ( ! source.isEmpty() ) {
-         inputDoc.setClientSource(source);
-      }
-      if ( !compiler.compileClientHeader() ) {
-         err << QCoreApplication::tr("Failed to compile the interface!") << endl;
-         return 1;
-      }
-      if ( !compiler.compileClientSource() ) {
-         err << QCoreApplication::tr("Failed to compile the interface!") << endl;
-         return 1;
-      }
-   } else {
-      err << QCoreApplication::tr("You should specify --%1 or --%2 flag!")
-                .arg(SERVICE_FLAG)
-                .arg(CLIENT_FLAG) << endl;
-      return 1;
-   }
+    /// @todo move output files to the compiler class.
+    QString header = conf.options()[HEADER_OPTION];
+    QString source = conf.options()[SOURCE_OPTION];
+    if (conf.flags()[SERVICE_FLAG] && conf.flags()[CLIENT_FLAG]) {
+        err << ArgvParser::tr("You should specify --%1 or --%2 flag but not both!")
+        .arg(SERVICE_FLAG).arg(CLIENT_FLAG) << endl;
+        return 1;
+    }
+    if (conf.flags()[SERVICE_FLAG]) {
+        if (!header.isEmpty())
+            inputDoc.setServiceHeader(header);
+        if (!source.isEmpty())
+            inputDoc.setServiceSource(source);
+        if (!compiler.compileServiceHeader()) {
+            err << QCoreApplication::tr("Failed to compile the interface!") << endl;
+            return 1;
+        }
+        if (!compiler.compileServiceSource()) {
+            err << QCoreApplication::tr("Failed to compile the interface!") << endl;
+            return 1;
+        }
+    } else if (conf.flags()[CLIENT_FLAG]) {
+        if (!header.isEmpty()) {
+            inputDoc.setClientHeader(header);
+        }
+        if (!source.isEmpty()) {
+            inputDoc.setClientSource(source);
+        }
+        if (!compiler.compileClientHeader()) {
+            err << QCoreApplication::tr("Failed to compile the interface!") << endl;
+            return 1;
+        }
+        if (!compiler.compileClientSource()) {
+            err << QCoreApplication::tr("Failed to compile the interface!") << endl;
+            return 1;
+        }
+    } else {
+        err << QCoreApplication::tr("You should specify --%1 or --%2 flag!")
+            .arg(SERVICE_FLAG)
+            .arg(CLIENT_FLAG) << endl;
+        return 1;
+    }
 
-   return 0;
+    return 0;
 }
